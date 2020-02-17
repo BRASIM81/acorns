62fa7c153ba1b4e12369d6442db08568fdd17e27
Simon <simonrbrain@yahoo.co.uk>
2020-02-17 15:15:37 +0000

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-(new WOW).init(),$(document).ready(function(){});
+(new WOW).init(),$(document).ready(function(){var e=(new Date).getFullYear();document.getElementById("copy").innerHTML="ACORNS "+e});
